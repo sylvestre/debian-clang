300d5a5d44f517b5f27e09ac3738e75d5e996c55
Sylvestre Ledru <sylvestre.ledru@scilab-enterprises.com>
2012-11-07 16:50:02 +0100

    script to kill any usage of gcc/g++/cpp

---
+++ gcc-replacement.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "The package has an explicit usage on gcc / g++"
+echo "Your package should use /usr/bin/cc / /usr/bin/c++"
+echo "More information on http://wiki.debian.org/TODO"
+exit 42
