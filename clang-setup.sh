b1de04d747946482f5e81b88ea74258a6ff7a8ac
Sylvestre Ledru <sylvestre.ledru@scilab-enterprises.com>
2012-11-27 19:00:37 +0100

    dpkg is already on hold

--- clang-setup.sh
+++ clang-setup.sh
@@ -3,5 +3,5 @@
 echo "apt-get -yf install clang"
 apt-get -yf install clang
 
-echo "Put dpkg on hold (http://buildd-clang.debian.net uses a patched version of dpkg)"
-echo "dpkg hold" | dpkg --set-selections
+#echo "Put dpkg on hold (http://buildd-clang.debian.net uses a patched version of dpkg)"
+#echo "dpkg hold" | dpkg --set-selections
