73ee1cdf96ef2b3ed6b24589f45aadccec819f27
Sylvestre Ledru <sylvestre.ledru@scilab-enterprises.com>
2013-04-07 14:56:53 +0200

    Update the configuration of clang to match the one used during the mass rebuild. The previous one is too hard

--- clang-setup.sh
+++ clang-setup.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-echo "apt-get -yf install clang"
-apt-get -yf install clang
+echo "deb http://ftp.fr.debian.org/debian/ experimental main" >> /etc/apt/sources.list
+
+echo "Install of clang"
+apt-get update
+aptitude -y --without-recommends install cpp g++ gcc clang -t experimental 
+#apt-get install --yes --no-install-recommends clang -t experimental
+
+echo "Replace gcc, g++ & cpp by clang"
+VERSIONS="4.8 4.7"
+for VERSION in $VERSIONS; do
+cd /usr/bin
+rm g++-$VERSION gcc-$VERSION cpp-$VERSION
+ln -s clang++ g++-$VERSION
+ln -s clang gcc-$VERSION
+ln -s clang cpp-$VERSION
+cd -
+
+echo "Block the installation of new gcc version $VERSION"
+echo "gcc-$VERSION hold"|dpkg --set-selections
+echo "cpp-$VERSION hold"|dpkg --set-selections
+echo "g++-$VERSION hold"|dpkg --set-selections
+done
+
+echo "Check if gcc, g++ & cpp are actually clang"
+gcc --version|grep clang > /dev/null || exit 1
 
 #echo "Put dpkg on hold (http://buildd-clang.debian.net uses a patched version of dpkg)"
 #echo "dpkg hold" | dpkg --set-selections
